29f2d735cadd798796b58394f5a08b518dc52478
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-08-04 15:57:00 +0530

    finalizing the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductTableComponent } from './product-table/product-table.component';
+import { OrderTableComponent } from './order-table/order-table.component';
+
+
+
+interface Product {
+  name: string;
+  quantity: number | string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductTableComponent,OrderTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'vijayi-technologies';
+
+  constructor(private http: HttpClient) { }
+  order: Product[] = [];
+
+  updateOrder(order: Product[]) {
+    this.order = order;
+  }
+
 }
+
+
